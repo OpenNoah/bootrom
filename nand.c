1e54da62465058669629152bf6923541dcff54e5
Norman Zhi <normanzyb@gmail.com>
2024-03-16 00:20:21 +0000

    D88: Add BCH ECC support for NAND filesystem pages

--- nand.c
+++ nand.c
@@ -56,6 +56,11 @@ static struct bch_t {
 } * const bch = BCH_BASE;
 #endif
 
+static struct {
+    uint32_t correctable;
+    uint32_t uncorrectable;
+} ecc_count = {0};
+
 static inline void nand_fce_assert(const unsigned bank)
 {
     uint32_t mask = 0x03 << (2 * (bank - 1));
@@ -137,8 +142,19 @@ void nand_read_pages(void *dst, uint32_t start, uint32_t count, int oob)
                 p[i] = *NAND_DATA_PORT_8(bank);
         }
 
+        // D88 uses BCH hardware ECC
         uint32_t ecc_ofs = start <= 3 ? 3 : 24;
         for (unsigned block = 0; block < config.nand.page / 512; block++) {
+            if (start >= 0x0800) {
+                // If page data is all 0xff, then skip ECC correction
+                // mtdblock may add oob data without ECC
+                uint32_t data_mask = 0xffffffff;
+                for (unsigned i = 0; i < 512 / 4; i++)
+                    data_mask &= page_data[i];
+                if (data_mask == 0xffffffff)
+                    continue;
+            }
+
             // BCH ECC decoding sequence
             // 1 Set BHCR.BCHE to 1 to enable BCH controller.
             // 2 Select 4-bit or 8-bit correction by setting BHCR.BSEL.
@@ -147,12 +163,16 @@ void nand_read_pages(void *dst, uint32_t start, uint32_t count, int oob)
             // 4 Set BHCR.BRST to 1 to reset BCH controller.
             bch->BHCSR = BIT(2) | BIT(1) | BIT(0);
             // 5 Set BHCNT.DEC_COUNT to data block size in bytes.
-            bch->BHCNT = (512 + 13) << 16;
+            bch->BHCNT = (512 + 13 + (start >= 0x0800 ? 3 : 0)) << 16;
             // Clear error flags
             bch->BHINT = 0xff;
             // 6 Byte-write all data block to BHDR.
             for (unsigned i = 0; i < 512; i++)
                 bch->BHDR = ((uint8_t *)&page_data[0])[block * 512 + i];
+            // For rootfs etc., include oob area data
+            if (start >= 0x0800)
+                for (unsigned i = 0; i < 3; i++)
+                    bch->BHDR = ((uint8_t *)&oob_data[0])[block * 3 + i];
             for (unsigned i = 0; i < 13; i++)
                 bch->BHDR = ((uint8_t *)&oob_data[0])[ecc_ofs + block * 13 + i];
             // 7 Check BHINTS.DECF bit or by enabling decoding finish interrupt.
@@ -161,10 +181,10 @@ void nand_read_pages(void *dst, uint32_t start, uint32_t count, int oob)
             uint32_t status = bch->BHINT;
             if (status & BIT(0)) {
                 // Error occured
-#if 1
+#if 0
                 uart_puts("BCH ECC status: 0x");
                 uart_puthex(bch->BHINT, 8);
-                uart_puts(", 0x");
+                uart_puts(": 0x");
                 uart_puthex(bch->BHERR[0], 8);
                 uart_puts(", 0x");
                 uart_puthex(bch->BHERR[1], 8);
@@ -182,46 +202,65 @@ void nand_read_pages(void *dst, uint32_t start, uint32_t count, int oob)
                 if (status & BIT(1)) {
                     // Uncorrectable error occured
                     uart_puts("Uncorrectable\r\n");
+                    ecc_count.uncorrectable += 1;
                 } else {
                     uint32_t err_bits = status >> 28;
                     uart_puthex(err_bits, 1);
                     uart_puts(" errors corrected\r\n");
+                    ecc_count.correctable += err_bits;
                     while (err_bits--) {
                         uint16_t bit = (bch->BHERR[err_bits / 2] >> (16 * (err_bits % 2))) - 1;
-                        ((uint8_t *)&page_data[0])[bit / 8] ^= 1 << (bit % 8);
+                        if (bit < 512 * 8) {
+                            // Error is in page data
+                            bit += block * 512 * 8;
+                            ((uint8_t *)&page_data[0])[bit / 8] ^= 1 << (bit % 8);
+                        } else if (start >= 0x0800 && bit < (512 + 3) * 8) {
+                            // Error is in oob data
+                            bit -= 512 * 8;
+                            bit += block * 3 * 8;
+                            ((uint8_t *)&oob_data[0])[bit / 8] ^= 1 << (bit % 8);
+                        } else {
+                            // Error is in parity data
+                            bit -= (512 + (start >= 0x0800 ? 3 : 0)) * 8;
+                            bit += (ecc_ofs + block * 13) * 8;
+                            ((uint8_t *)&oob_data[0])[bit / 8] ^= 1 << (bit % 8);
+                        }
                     }
                 }
-            }
 
 #if 0
-            // BCH ECC encoding sequence
-            // 1 Set BHCR.BCHE to 1 to enable BCH controller.
-            // 2 Select 4-bit or 8-bit correction by setting BHCR.BSEL.
-            // 3 Set BHCR.ENCE to 1 to enable encoding.
-            // 4 Set BHCR.BRST to 1 to reset BCH controller.
-            bch->BHCSR = BIT(3) | BIT(2) | BIT(1) | BIT(0);
-            // 5 Set BHCNT.ENC_COUNT to data block size in bytes.
-            bch->BHCNT = 512;
-            // Clear error flags
-            bch->BHINT = 0xff;
-            // 6 Byte-write all data block to BHDR.
-            for (unsigned i = 0; i < 512; i++)
-                bch->BHDR = ((uint8_t *)&page_data[0])[block * 512 + i];
-            // 7 Check BHINTS.ENCF bit or by enabling encoding finish interrupt.
-            while (!(bch->BHINT & BIT(2)));
-            // 8 When encoding finishes, read out the parity data in BHPARn.
-            uart_puts("BCH ECC encoding: 0x");
-            uart_puthex(bch->BHINT, 8);
-            uart_puts(", 0x");
-            uart_puthex(bch->BHPAR[0], 8);
-            uart_puts(", 0x");
-            uart_puthex(bch->BHPAR[1], 8);
-            uart_puts(", 0x");
-            uart_puthex(bch->BHPAR[2], 8);
-            uart_puts(", 0x");
-            uart_puthex(bch->BHPAR[3], 8);
-            uart_puts("\r\n");
+                // BCH ECC encoding sequence
+                // 1 Set BHCR.BCHE to 1 to enable BCH controller.
+                // 2 Select 4-bit or 8-bit correction by setting BHCR.BSEL.
+                // 3 Set BHCR.ENCE to 1 to enable encoding.
+                // 4 Set BHCR.BRST to 1 to reset BCH controller.
+                bch->BHCSR = BIT(3) | BIT(2) | BIT(1) | BIT(0);
+                // 5 Set BHCNT.ENC_COUNT to data block size in bytes.
+                bch->BHCNT = 512 + (start >= 0x0800 ? 3 : 0);
+                // Clear error flags
+                bch->BHINT = 0xff;
+                // 6 Byte-write all data block to BHDR.
+                for (unsigned i = 0; i < 512; i++)
+                    bch->BHDR = ((uint8_t *)&page_data[0])[block * 512 + i];
+                if (start >= 0x0800)
+                    for (unsigned i = 0; i < 3; i++)
+                        bch->BHDR = ((uint8_t *)&oob_data[0])[block * 3 + i];
+                // 7 Check BHINTS.ENCF bit or by enabling encoding finish interrupt.
+                while (!(bch->BHINT & BIT(2)));
+                // 8 When encoding finishes, read out the parity data in BHPARn.
+                uart_puts("BCH ECC encoding: 0x");
+                uart_puthex(bch->BHINT, 8);
+                uart_puts(": 0x");
+                uart_puthex(bch->BHPAR[0], 8);
+                uart_puts(", 0x");
+                uart_puthex(bch->BHPAR[1], 8);
+                uart_puts(", 0x");
+                uart_puthex(bch->BHPAR[2], 8);
+                uart_puts(", 0x");
+                uart_puthex(bch->BHPAR[3], 8);
+                uart_puts("\r\n");
 #endif
+            }
         }
 
         for (unsigned i = 0; i < config.nand.page / 4; i++)
@@ -231,4 +270,10 @@ void nand_read_pages(void *dst, uint32_t start, uint32_t count, int oob)
                 *buf32++ = oob_data[i];
         start++;
     }
+
+    uart_puts("ECC correctable: 0x");
+    uart_puthex(ecc_count.correctable, 8);
+    uart_puts(", uncorrectable: 0x");
+    uart_puthex(ecc_count.uncorrectable, 8);
+    uart_puts("\r\n");
 }
